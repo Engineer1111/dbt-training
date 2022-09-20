e4721818029b0949fd63de8dc21f3a71583c23d4
hb.shah@gmail.com <hb.shah@gmail.com>
2022-09-20 06:52:14 +0000

    commit

--- models/reporting/report_profit_by_product.sql
+++ models/reporting/report_profit_by_product.sql
@@ -1,12 +1,2 @@
-select 
-
-orderid,
-orderdate,
-sum(orderprofit) as orderprofit
---productname
-from {{ ref('stg_orders') }}
---category
---subcategory,
---sum(orderprofit) as profit
-group by 1,2
---group by productid
+select *
+from {{ ref('stg_orders1') }}
