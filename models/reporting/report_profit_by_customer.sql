643a0b40345a1725952ab8555b0403fb12c17b11
hb.shah@gmail.com <hb.shah@gmail.com>
2022-09-25 21:39:59 +0000

    customer

---
+++ models/reporting/report_profit_by_customer.sql
@@ -0,0 +1,10 @@
+select 
+customerid ,
+ customername
+,segment
+,country 
+,sum(orderprofit) as profit
+, count(1) as ct_entries
+, sum(orderprofit)/count(1) as avg_profit 
+from {{ ref('stg_orders') }}
+group by 1,2,3,4
