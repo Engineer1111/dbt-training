64c9d0721e60e73371dd387dcbc316edb36e2ad3
hb.shah@gmail.com <hb.shah@gmail.com>
2022-09-25 21:54:29 +0000

    deleted unwanted file

--- models/staging/stg_orders1.sql
+++
@@ -1,9 +0,0 @@
-select o.*
-, c.*
-, p.*
-from
-{{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c
-on (o.customerid = c.customerid)
-left join {{ ref('raw_product') }} as p 
-on (o.productid = p.productid)
