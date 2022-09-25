c8b1bcacb81dc1efde4b71e367870f07e9ced967
hb.shah@gmail.com <hb.shah@gmail.com>
2022-09-25 21:31:17 +0000

    added report_profit_by_product

--- models/reporting/report_profit_by_product.sql
+++ models/reporting/report_profit_by_product.sql
@@ -1,2 +1,10 @@
-select *
-from {{ ref('stg_orders1') }}
+select 
+productid
+,product_name
+,category
+,subcategory
+,sum(orderprofit) as profit
+, count(1) as ct_entries
+, sum(orderprofit)/count(1) as avg_profit 
+from {{ ref('stg_orders') }}
+group by 1,2,3,4
